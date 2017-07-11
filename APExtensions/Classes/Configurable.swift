301a8dff06ccd8d0b8d267d301b0d7e93ef69ae5
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2017-07-11 14:45:37 +0300

    - Configurable protocol

---
+++ APExtensions/Classes/Configurable.swift
@@ -0,0 +1,14 @@
+//
+//  Configurable.swift
+//  Direct Booking
+//
+//  Created by mac-246 on 6/16/17.
+//  Copyright © 2017 Convene. All rights reserved.
+//
+
+import Foundation
+
+
+protocol Configurable {
+    func configure(model: Any)
+}
