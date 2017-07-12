e365ba90ec6847a0177bd03e2fdf819a9af75bb8
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2017-07-12 18:34:15 +0300

    - Instantiate with owner

--- APExtensions/Classes/Instantiatable.swift
+++ APExtensions/Classes/Instantiatable.swift
@@ -11,16 +11,23 @@ import UIKit
 
 public protocol Instantiatable {
     static func instantiate() -> Self
+    static func instantiate(owner: Any) -> Self
 }
 
 public extension Instantiatable where Self: NSObject {
-    private final static func objectFromXib<T>() -> T {
-        return UINib(nibName: className, bundle: nil).instantiate(withOwner: nil, options: nil).first as! T
+    private final static func objectFromXib<T>(owner: Any?) -> T {
+        return UINib(nibName: className, bundle: nil).instantiate(withOwner: owner, options: nil).first as! T
     }
     
     /// Instantiates object from xib file. 
     /// Xib filename should be equal to object class name.
     public final static func instantiate() -> Self {
-        return objectFromXib()
+        return objectFromXib(owner: nil)
+    }
+    
+    /// Instantiates object from xib file with specified owner.
+    /// Xib filename should be equal to object class name.
+    public final static func instantiate(owner: Any) -> Self {
+        return objectFromXib(owner: owner)
     }
 }
