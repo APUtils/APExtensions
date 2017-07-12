f79dc80b3effa7741f56e50f68eb3735b2967cf9
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2017-07-12 20:19:23 +0300

    - New protocol to instantiate content views

--- APExtensions/Classes/Instantiatable.swift
+++ APExtensions/Classes/Instantiatable.swift
@@ -8,26 +8,38 @@
 
 import UIKit
 
+//-----------------------------------------------------------------------------
+// MARK: - Instantiatable
+//-----------------------------------------------------------------------------
 
 public protocol Instantiatable {
     static func instantiate() -> Self
-    static func instantiate(owner: Any) -> Self
 }
 
 public extension Instantiatable where Self: NSObject {
-    private final static func objectFromXib<T>(owner: Any?) -> T {
-        return UINib(nibName: className, bundle: nil).instantiate(withOwner: owner, options: nil).first as! T
+    private final static func objectFromXib<T>() -> T {
+        return UINib(nibName: className, bundle: nil).instantiate(withOwner: nil, options: nil).first as! T
     }
     
     /// Instantiates object from xib file. 
     /// Xib filename should be equal to object class name.
     public final static func instantiate() -> Self {
-        return objectFromXib(owner: nil)
+        return objectFromXib()
     }
-    
-    /// Instantiates object from xib file with specified owner.
-    /// Xib filename should be equal to object class name.
-    public final static func instantiate(owner: Any) -> Self {
-        return objectFromXib(owner: owner)
+}
+
+//-----------------------------------------------------------------------------
+// MARK: - InstantiatableContentView
+//-----------------------------------------------------------------------------
+
+public protocol InstantiatableContentView {
+    static func instantiateContentView() -> UIView
+}
+
+public extension InstantiatableContentView where Self: NSObject {
+    /// Instantiates contentView from xib file and making instantiator it's owner.
+    /// Xib filename should be composed of className + "ContentView" postfix. E.g. MyView -> MyViewContentView
+    public final static func instantiateContentView() -> UIView {
+        return UINib(nibName: "\(className)ContentView", bundle: nil).instantiate(withOwner: self, options: nil).first as! UIView
     }
 }
