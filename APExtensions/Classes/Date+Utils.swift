38406649cf48aee55a123241fefb09355ea02b08
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2017-07-19 13:49:48 +0300

    - More Date utils

--- APExtensions/Classes/Date+Utils.swift
+++ APExtensions/Classes/Date+Utils.swift
@@ -13,27 +13,39 @@ import Foundation
 //-----------------------------------------------------------------------------
 
 public extension Date {
-    /// Get today's date
+    /// Get today's date. Uses user's time zone.
     public static var today: Date {
         return Calendar.current.startOfDay(for: Date())
     }
     
-    /// Get date's components
-    public var components: DateComponents {
-        return Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: self)
+    /// Get day start. Uses user's time zone.
+    public var startOfDay: Date {
+        return Calendar.current.startOfDay(for: self)
     }
     
-    /// Checks if date is in today's range
+    /// Get date's components. Uses user's time zone.
+    public var components: DateComponents {
+        return Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second, .timeZone, .calendar], from: self)
+    }
+    
+    /// Checks if date is in today's range. Uses user's time zone.
     public var isToday: Bool {
         return Calendar.current.isDateInToday(self)
     }
     
-    /// Checks if date is in tomorrow's range
+    /// Checks if date is in tomorrow's range. Uses user's time zone.
     public var isTomorrow: Bool {
         return Calendar.current.isDateInTomorrow(self)
     }
     
-    /// Checks if dates are on same day
+    /// Converts date to GMT time zone day start.
+    public var gmtDayBeginningDate: Date {
+        let toGmtTimeInterval = TimeInterval(TimeZone.current.secondsFromGMT(for: self))
+        
+        return Date(timeInterval: toGmtTimeInterval, since: self)
+    }
+    
+    /// Checks if dates are on same day. Uses user's time zone.
     public func isSameDay(withDate date: Date) -> Bool {
         return Calendar.current.isDate(self, inSameDayAs: date)
     }
