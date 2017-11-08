2ef8f6033fdab18cbd4ce30a36a3d4ae2563b962
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2017-11-08 18:28:30 +0300

    - APUtils -perform method now catches C++ exceptions

--- APExtensions/Classes/Core/APUtils.h
+++ APExtensions/Classes/Core/APUtils.h
@@ -2,15 +2,14 @@
 //  APUtils.h
 //  APExtensions
 //
-//  Created by mac-246 on 11/3/17.
+//  Created by Anton Plebanovich on 11/3/17.
+//  Copyright © 2017 Anton Plebanovich. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
-
 @interface APUtils : NSObject
 + (NSException * _Nullable)perform:(__attribute__((noescape)) void (^)(void))block;
 @end
-
 NS_ASSUME_NONNULL_END
