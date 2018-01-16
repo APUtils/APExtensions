b5339d56c1fa095bf57ed9fdd003574b8dfffd7f
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2018-01-16 12:11:37 +0300

    String .base64 -> .encodedBase64 String .decodedBase64

--- APExtensions/Classes/Core/_Extensions/String+Utils.swift
+++ APExtensions/Classes/Core/_Extensions/String+Utils.swift
@@ -32,12 +32,19 @@ public extension String {
     }
 }
 
-// ******************************* MARK: - Convertation
+// ******************************* MARK: - Base64
 
 public extension String {
     /// Returns base64 encoded string
-    public var base64: String? {
+    public var encodedBase64: String? {
         return data(using: .utf8)?.base64EncodedString()
+    }
+    
+    /// Returns string decoded from base64 string
+    public var decodedBase64: String? {
+        guard let data = Data(base64Encoded: self) else { return nil }
+        
+        return String(data: data, encoding: .utf8)
     }
 }
 
