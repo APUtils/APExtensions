6b91925c203511ac092cc40e7cdb78cad74597cb
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2018-01-23 23:57:20 +0300

    - AlertController.presentationStyle - AlertController .present(animated:) -> present(animated:completion:) - UIWebView .load(urlString:) - UIWebView .load(url:) - UIWebView .clear() - WKWebView .load(_ string:) - WKWebView .load(_ url:) - WKWebView .clear()

--- APExtensions/Classes/Core/_Controllers/AlertController.swift
+++ APExtensions/Classes/Core/_Controllers/AlertController.swift
@@ -27,6 +27,20 @@ fileprivate final class AlertRootViewController: UIViewController {
 
 public final class AlertController: UIAlertController {
     
+    // ******************************* MARK: - Types
+    
+    public enum PresentationStyle {
+        /// Present in separate window
+        case window
+        
+        /// Present in key window from top presented controller
+        case topController
+    }
+    
+    // ******************************* MARK: - Classes Properties
+    
+    public static var presentationStyle: PresentationStyle = .window
+    
     // ******************************* MARK: - Private Properties
     
     private lazy var rootVC: AlertRootViewController = {
@@ -65,10 +79,16 @@ public final class AlertController: UIAlertController {
     
     // ******************************* MARK: - Public Methods
     
-    public func present(animated: Bool) {
+    public func present(animated: Bool, completion: (() -> Void)? = nil) {
         g_performInMain {
-            self.alertWindow?.makeKeyAndVisible()
-            self.alertWindow?.rootViewController?.present(self, animated: animated, completion: nil)
+            switch AlertController.presentationStyle {
+            case .window:
+                self.alertWindow?.makeKeyAndVisible()
+                self.alertWindow?.rootViewController?.present(self, animated: animated, completion: completion)
+                
+            case .topController:
+                g_topViewController?.present(self, animated: animated, completion: completion)
+            }
         }
     }
 }
