cae22153f8c300f66550dc638499eca7316fcacb
Anton Plebanovich <anton.plebanovich@gmail.com>
2019-01-19 21:33:23 +0300

    - SingletonManager

---
+++ APExtensions/Classes/Core/_Protocols/SingletonManager.swift
@@ -0,0 +1,16 @@
+//
+//  SingletonManager.swift
+//  Elevate
+//
+//  Created by Anton Plebanovich on 2/28/18.
+//  Copyright © 2018 Anton Plebanovich. All rights reserved.
+//
+
+import Foundation
+
+
+/// Same as Manager but with singleton class property
+protocol SingletonManager: Manager {
+    /// Manager singleton
+    static var shared: Self { get }
+}
