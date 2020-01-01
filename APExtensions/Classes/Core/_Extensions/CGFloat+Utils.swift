e9713e177c6608414430bd20fea469662f4cb94a
Anton Plebanovich <anton.plebanovich@gmail.com>
2020-01-01 16:37:37 +0300

    - CGFloat .asString - CGFloat .asTenthString - CGFloat .asHundredthString - CGFloat .asInt - CGFloat .asDouble

---
+++ APExtensions/Classes/Core/_Extensions/CGFloat+Utils.swift
@@ -0,0 +1,48 @@
+//
+//  CGFloat+Utils.swift
+//  APExtensions-example
+//
+//  Created by Anton Plebanovich on 1/1/20.
+//  Copyright © 2020 Anton Plebanovich. All rights reserved.
+//
+
+import Foundation
+
+// ******************************* MARK: - As String
+
+public extension CGFloat {
+    
+    /// Returns `self` as `String`
+    var asString: String {
+        return asDouble.asString
+    }
+    
+    /// Returns string representation rounded to tenth
+    var asTenthString: String {
+        return String(format: "%.1f", self)
+    }
+    
+    /// Returns string representation rounded to hundredth
+    var asHundredthString: String {
+        return String(format: "%.2f", self)
+    }
+}
+
+// ******************************* MARK: - As
+
+public extension CGFloat {
+    
+    /// Returns `self` as `Int` if possible
+    var asInt: Int? {
+        if self >= Int.min.asCGFloat, self < Int.max.asCGFloat {
+            return Int(self)
+        } else {
+            return nil
+        }
+    }
+    
+    /// Returns `self` as `Double`
+    var asDouble: Double {
+        return Double(self)
+    }
+}
