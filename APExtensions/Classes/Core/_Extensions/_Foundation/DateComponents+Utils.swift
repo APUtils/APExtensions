812abaf8abbbc42ee1e29a22c330f344d084faf6
Anton Plebanovich <anton.plebanovich@gmail.com>
2020-01-06 20:50:18 +0300

    - DateComponents .init(timeInterval:) -> .init(timeInterval:components:)

--- APExtensions/Classes/Core/_Extensions/_Foundation/DateComponents+Utils.swift
+++ APExtensions/Classes/Core/_Extensions/_Foundation/DateComponents+Utils.swift
@@ -10,13 +10,12 @@ import Foundation
 
 public extension DateComponents {
     
-    /// Initializes date components with provided `tiveInterval`.
-    /// Uses day, hour, minute, second, nanosecond components.
-    init(timeInterval: TimeInterval) {
+    /// Initializes date components with provided `tiveInterval` and `components`.
+    /// Uses day, hour, minute, second, nanosecond components by default.
+    init(timeInterval: TimeInterval, components: Set<Calendar.Component> = [.day, .hour, .minute, .second, .nanosecond]) {
         let calendar = Calendar(identifier: .gregorian)
         let date1 = Date()
         let date2 = Date(timeInterval: timeInterval, since: date1)
-        let components: Set<Calendar.Component> = [.day, .hour, .minute, .second, .nanosecond]
         self = calendar.dateComponents(components, from: date1, to: date2)
     }
 }
