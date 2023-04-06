f8c12db45650825cf10bebc374a148e448fdff1e
Anton Plebanovich <anton.plebanovich@gmail.com>
2023-04-06 16:49:12 +0600

    [Error] isGeneralError [Error] asStringError [GeneralError] shared [StringError] public message property

--- APExtensions/Classes/Core/Definitions.swift
+++ APExtensions/Classes/Core/Definitions.swift
@@ -22,11 +22,14 @@ public typealias ErrorClosure = (_ error: Error?) -> Void
 // ******************************* MARK: - Error
 
 /// Error stub to use for simplification
-public struct GeneralError: Error { public init() {} }
+public struct GeneralError: Error {
+    public static let shared = GeneralError()
+    init() {}
+}
 
 /// Error with message string used as error description
 public struct StringError: LocalizedError, CustomStringConvertible {
-    private let message: String
+    public let message: String
     public init(_ message: String) { self.message = message }
     
     // LocalizedError
@@ -34,4 +37,17 @@ public struct StringError: LocalizedError, CustomStringConvertible {
     
     // CustomStringConvertible
     public var description: String { message }
+}
+
+public extension Error {
+    
+    /// Checks if error is `GeneralError`
+    var isGeneralError: Bool {
+        self is GeneralError
+    }
+    
+    /// Returns `self` as `StringError`
+    var asStringError: StringError? {
+        self as? StringError
+    }
 }
