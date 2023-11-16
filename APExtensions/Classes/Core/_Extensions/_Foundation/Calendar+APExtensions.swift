2b45ee58c5a32daecdc1f1b8d68555944b30ca2e
Anton Plebanovich <anton.plebanovich@gmail.com>
2023-11-16 22:51:43 +0600

    - [Calendar] gregorian

---
+++ APExtensions/Classes/Core/_Extensions/_Foundation/Calendar+APExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  Calendar+APExtensions.swift
+//  Pods
+//
+//  Created by Anton Plebanovich on 16.11.23.
+//  Copyright © 2023 Anton Plebanovich. All rights reserved.
+//
+
+import Foundation
+
+public extension Calendar {
+    
+    /// Gregorian calendar
+    static let gregorian: Calendar = Calendar(identifier: .gregorian)
+}
