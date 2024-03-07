041096a2e82a830b71932c624eaf8b4260f30425
Anton Plebanovich <anton.plebanovich@gmail.com>
2024-03-07 14:52:55 +0600

    - [JSONSerialization] Error logs data fix

--- APExtensions/Classes/Core/_Extensions/_Foundation/JSONSerialization+AP.swift
+++ APExtensions/Classes/Core/_Extensions/_Foundation/JSONSerialization+AP.swift
@@ -14,14 +14,14 @@ public extension JSONSerialization {
     static func safeJSONData(object: Any, options: JSONSerialization.WritingOptions, file: String = #file, function: String = #function, line: UInt = #line) -> Data? {
         
         guard isValidJSONObject(self) else {
-            RoutableLogger.logError("Ivalid JSON object", data: ["self": self, "options": options], file: file, function: function, line: line)
+            RoutableLogger.logError("Ivalid JSON object", data: ["object": object, "options": options], file: file, function: function, line: line)
             return nil
         }
         
         do {
             return try data(withJSONObject: self, options: options)
         } catch {
-            RoutableLogger.logError("Unable to transform object to JSON string", error: error, data: ["self": self, "options": options], file: file, function: function, line: line)
+            RoutableLogger.logError("Unable to transform object to JSON string", error: error, data: ["object": object, "options": options], file: file, function: function, line: line)
             return nil
         }
     }
