69dd4998520f179beed8e8f39a12335dd26ca829
Anton Plebanovich <anton.plebanovich@gmail.com>
2025-03-05 14:32:58 +0600

    - [NSObject] `doOnce` rework into `Int` type key

--- APExtensions/Classes/Core/_Extensions/_Foundation/NSObject+Utils.swift
+++ APExtensions/Classes/Core/_Extensions/_Foundation/NSObject+Utils.swift
@@ -13,9 +13,9 @@ import Foundation
 private var c_doOnceStorageAssociationKey = 0
 
 public extension NSObject {
-    private var doOnceStorage: [String]? {
+    private var doOnceStorage: Set<Int>? {
         get {
-            return objc_getAssociatedObject(self, &c_doOnceStorageAssociationKey) as? [String]
+            return objc_getAssociatedObject(self, &c_doOnceStorageAssociationKey) as? Set<Int>
         }
         set {
             objc_setAssociatedObject(self, &c_doOnceStorageAssociationKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
@@ -23,13 +23,13 @@ public extension NSObject {
     }
     
     /// Performs action for a key once only.
-    func doOnce(key: String, action: () -> Void) {
+    func doOnce(key: Int = #line, action: () -> Void) {
         var shouldPerformAction = false
         g.synchronized(self) {
             var doOnceStorage = self.doOnceStorage ?? []
             guard !doOnceStorage.contains(key) else { return }
             
-            doOnceStorage.append(key)
+            doOnceStorage.insert(key)
             self.doOnceStorage = doOnceStorage
             shouldPerformAction = true
         }
