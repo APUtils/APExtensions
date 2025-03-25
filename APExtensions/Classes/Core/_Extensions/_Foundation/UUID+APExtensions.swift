fb7a839c7bc89d693a202e9157c132ca76658447
Anton Plebanovich <anton.plebanovich@gmail.com>
2025-03-25 19:51:44 +0600

    - [UUID] `zero` - [UUID] `isZero`

--- APExtensions/Classes/Core/_Extensions/_Foundation/UUID+APExtensions.swift
+++ APExtensions/Classes/Core/_Extensions/_Foundation/UUID+APExtensions.swift
@@ -10,6 +10,14 @@ import Foundation
 
 public extension UUID {
     
+    /// `00000000-0000-0000-0000-000000000000`
+    static let zero: UUID = UUID(uuidString: "00000000-0000-0000-0000-000000000000")!
+    
+    /// `true` if `self` is `00000000-0000-0000-0000-000000000000`
+    var isZero: Bool {
+        self == Self.zero
+    }
+    
     /// Returns `uuidString` without hyphens
     var uuidStringWithoutHyphens: String {
         uuidString.replacingOccurrences(of: "-", with: "")
