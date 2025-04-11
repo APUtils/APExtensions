9476102a11de25a5f725fbec379d21f1831985d1
Anton Plebanovich <anton.plebanovich@gmail.com>
2025-04-11 14:05:08 +0600

    - [CharacterSet] `printDescription()` and `asArray()` DEBUG methods

--- APExtensions/Classes/Core/_Extensions/_Foundation/CharacterSet+AP.swift
+++ APExtensions/Classes/Core/_Extensions/_Foundation/CharacterSet+AP.swift
@@ -10,6 +10,27 @@ import Foundation
 
 public extension CharacterSet {
     
+#if DEBUG
+    func printDescription() {
+        let array = asArray()
+        print("Count: \(array.count)")
+        for scalar in array {
+            print("\(scalar) (U+\(String(scalar.value, radix: 16, uppercase: true)))")
+        }
+    }
+    
+    func asArray() -> [Unicode.Scalar] {
+        var array = [Unicode.Scalar]()
+        for codePoint in (0 as UInt32) ... 0x10FFFF {
+            if let scalar = Unicode.Scalar(codePoint),
+               self.contains(scalar) {
+                array.append(scalar)
+            }
+        }
+        return array
+    }
+#endif
+    
     func containsUnicodeScalars(of character: Character) -> Bool {
         return character.unicodeScalars.allSatisfy(contains(_:))
     }
