5db185136470a8ab3855972b034a3d246fd1d1f2
Anton Plebanovich <anton.plebanovich@gmail.com>
2025-07-17 20:17:44 +0600

    - [NSString] `asString`

---
+++ APExtensions/Classes/Core/_Extensions/_Foundation/NSString+APExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  NSString+APExtensions.swift
+//  Pods
+//
+//  Created by Anton Plebanovich on 17.07.25.
+//  Copyright © 2025 Anton Plebanovich. All rights reserved.
+//
+
+import Foundation
+
+public extension NSString {
+    
+    /// Returns `self` as `String`
+    var asString: String {
+        String(self)
+    }
+}
