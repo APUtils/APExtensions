0b08efe86aa806d1e54a7c2a8848ac1e42fd5ac6
Anton Plebanovich <anton.plebanovich@gmail.com>
2025-07-18 20:04:43 +0600

    - [CFString] `asString`

---
+++ APExtensions/Classes/Core/_Extensions/_CoreFoundation/CFString+APExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  CFString+APExtensions.swift
+//  Pods
+//
+//  Created by Anton Plebanovich on 18.07.25.
+//  Copyright © 2025 Anton Plebanovich. All rights reserved.
+//
+
+import CoreFoundation
+
+// ******************************* MARK: - As
+
+extension CFString {
+    var asString: String {
+        String(self)
+    }
+}
