78215ab8bf6813de7e98cbdd796734797860bbaa
Anton Plebanovich <anton.plebanovich@gmail.com>
2025-08-14 11:21:23 +0600

    - [Sequence] `dictionaryCompactMapKeysAndMapValues` interface fix

--- APExtensions/Classes/Core/_Extensions/_Swift/Sequence+Utils.swift
+++ APExtensions/Classes/Core/_Extensions/_Swift/Sequence+Utils.swift
@@ -142,7 +142,7 @@ public extension Sequence {
     }
     
     /// Transforms an array to a dictionary using key and value transforms. It filter `nil` keys.
-    @inlinable func dictionaryCompactMapKeysAndMapValues<K, V>(keyTransform: (_ element: Iterator.Element) throws -> K?, valueTransform: (_ element: Iterator.Element) throws -> V?) rethrows -> [K: V?] {
+    @inlinable func dictionaryCompactMapKeysAndMapValues<K, V>(keyTransform: (_ element: Iterator.Element) throws -> K?, valueTransform: (_ element: Iterator.Element) throws -> V) rethrows -> [K: V] {
         return try reduce(into: [K: V]()) { dictionary, element in
             guard let key = try keyTransform(element) else { return }
             dictionary[key] = try valueTransform(element) 
