dcb3fa646b3dbf5c6cfc6d37564562a07b4213e3
Anton Plebanovich <anton.plebanovich@gmail.com>
2025-09-07 18:25:49 +0600

    - Properly inset content view

--- APExtensions/Classes/Core/_Protocols/Instantiatable.swift
+++ APExtensions/Classes/Core/_Protocols/Instantiatable.swift
@@ -102,7 +102,7 @@ public extension InstantiatableContentView where Self: UIView {
     func instantiateAndAttachContentView(insets: UIEdgeInsets = .zero, bottomPriority: UILayoutPriority = .required) {
         backgroundColor = .clear
         let contentView = instantiateContentView()
-        contentView.frame = bounds
+        contentView.frame = bounds.inset(by: insets)
         addSubview(contentView)
         contentView.constraintSides(to: self, insets: insets, bottomPriority: bottomPriority)
     }
